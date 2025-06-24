7b927443bdc308e2be83fd5ef437e072c2ef3ab0
root <root@tpotilli.tpotilli.virtualbox.org>
2025-06-24 20:55:30 +0200

    push mardi soir

--- srcs/requirement/mariadb/conf/config.sh
+++ srcs/requirement/mariadb/conf/config.sh
@@ -1,4 +1,6 @@
-service mysql start; # change mariadb en mysql
+#!/bin/bash
+
+service mysql start;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
